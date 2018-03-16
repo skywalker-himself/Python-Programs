c750704ecd72be2655d5d6dfe36628189e567ef3
Daniel <34347861+skywalker-himself@users.noreply.github.com>
2018-03-16 10:41:47 -0400

    Update Kilometers to Miles.py

--- Kilometers to Miles.py
+++ Kilometers to Miles.py
@@ -1,13 +1,17 @@
 def KM2Miles(kiloDist):
+    #Converts kilometers to miles
     milesDist = kiloDist*0.6214
+    #Returns the distance in miles, rounded to the 2nd decimal point
     return(round(milesDist,2))
 
 def main():
     choice = "y"
     while choice == "y":
         
+        #Prompts the user to input a distance in kilometers and stores it in variable kiloDist
         kiloDist = float(input("Enter distance in kilometers: "))
         
+        #Passes value to function KM2Miles
         distMI = KM2Miles(kiloDist) #Variables can be passed into the function
         print(distMI)
 

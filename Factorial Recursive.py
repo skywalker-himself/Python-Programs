2e9f3a1624b2f0bfb92e5c63a1f0114b4620915b
Daniel <34347861+skywalker-himself@users.noreply.github.com>
2018-03-16 10:36:07 -0400

    Update Factorial Recursive.py

--- Factorial Recursive.py
+++ Factorial Recursive.py
@@ -1,18 +1,23 @@
-def calculate_factorial(posINT):
-    x = 0
+def calculate_Factorial(posINT):
+    #Sets initial condition
     if posINT == 1:
        print(posINT)
        return posINT
+    #Sets recursive condition by calling function calculate_Factorial
     else:
-        x = posINT * calculate_factorial(posINT - 1)
+        x = posINT * calculate_Factorial(posINT - 1)
         print(x)
         return x
+    
 def main():
     choice = "y"
     while choice == "y":
         
+        #Prompts the user to enter a positive integer, and stores it in variable posINT
         posINT = int(input("Enter a positive integer: "))
-        functionCallVAR = calculate_factorial(posINT) #Variables can be passed into the function
+        
+        #Passes the value to function calculate_Factorial
+        functionCallVAR = calculate_Factorial(posINT) #Variables can be passed into the function
 
         print()
         choice = str(input("Would you like to run it again?: (y/n)  "))

06e2edb67c07ac8b690e9471a1bc7482aacc87ac
Daniel <34347861+skywalker-himself@users.noreply.github.com>
2018-03-19 21:03:23 -0400

    Update Reverse Pyramid.py

--- Reverse Pyramid.py
+++ Reverse Pyramid.py
@@ -1,5 +1,7 @@
 def main():
+    #This for loop sets up how many rows of *'s there will be
     for n in range(0,7):
+        #This for loop displays the number of *'s that should be displayed per row
         for i in range(7, n, -1):
             print("* ", end="")
         print()
